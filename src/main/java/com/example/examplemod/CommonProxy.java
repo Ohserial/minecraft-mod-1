e4f508bd0bcc7c8dd057d48844472fea6fa44bfe
Ohserial <gabrielrgerman@gmail.com>
2021-03-22 20:32:02 +0000

    Import error 3

--- src/main/java/com/example/examplemod/CommonProxy.java
+++ src/main/java/com/example/examplemod/CommonProxy.java
@@ -4,8 +4,9 @@ import net.minecraft.item.Item;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraft.client.model.renderer.block.ModelResourceLocation;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
 
 @Mod.EventBusSubscriber
 public class CommonProxy {
