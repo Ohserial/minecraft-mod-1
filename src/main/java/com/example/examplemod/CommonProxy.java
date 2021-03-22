03295715c03ee3580033368aef7774779295a181
Ohserial <gabrielrgerman@gmail.com>
2021-03-22 20:21:49 +0000

    Import error

--- src/main/java/com/example/examplemod/CommonProxy.java
+++ src/main/java/com/example/examplemod/CommonProxy.java
@@ -4,6 +4,8 @@ import net.minecraft.item.Item;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.client.model.ModelResourceLocation;
+import net.minecraftforge.client.model.ModelLoader;
 
 @Mod.EventBusSubscriber
 public class CommonProxy {
